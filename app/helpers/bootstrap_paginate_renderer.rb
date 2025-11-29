fa4ee307d32b21be61e9c701cb4c3db6c8ad1f57
akg807 <akshatgarg110@gmail.com>
2025-11-30 02:58:03 +0530

    finish user edit, update, index, and destroy actions

---
+++ app/helpers/bootstrap_paginate_renderer.rb
@@ -0,0 +1,35 @@
+class BootstrapPaginateRenderer < WillPaginate::ActionView::LinkRenderer
+  def container_attributes
+    { class: "pagination justify-content-center" }
+  end
+
+  protected
+
+  def html_container(html)
+    tag(:nav, super(html))
+  end
+
+  def page_number(page)
+    classes = "page-item"
+    classes << " active" if page == current_page
+
+    tag(:li, class: classes) do
+      link(page, page, class: "page-link")
+    end
+  end
+
+  def previous_or_next_page(page, text, classname)
+    classes = "page-item"
+    classes << " disabled" unless page
+
+    tag(:li, class: classes) do
+      link(text, page || "#", class: "page-link")
+    end
+  end
+
+  def gap
+    tag(:li, class: "page-item disabled") do
+      tag(:span, "...", class: "page-link")
+    end
+  end
+end
